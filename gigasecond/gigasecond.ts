bdf217845cf6a9bbeab5e0e38740c46c0540c642
Alexandro Perez <apgfriend@gmail.com>
2021-07-29 13:05:38 -0500

    solve gigasecond

--- gigasecond/gigasecond.ts
+++ gigasecond/gigasecond.ts
@@ -1,7 +1,16 @@
 class Gigasecond {
-  date(/* Parameters go here */) {
-    // Your code here
+  // Your code here
+  _date: Date;
+
+  constructor(date: Date) {
+    this._date = date;
+  }
+
+  date(): Date {
+    const gigasecond: number = Math.pow(10, 9);
+    //              date in miliseconds    seconds      miliseconds
+    return new Date(this._date.getTime() + gigasecond * 1000);
   }
 }
 
-export default Gigasecond
+export default Gigasecond;
